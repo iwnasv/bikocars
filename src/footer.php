821789011cac2504a3de0bd7ffd6413b89b66ae6
iwnasv <34813745+iwnasv@users.noreply.github.com>
2023-06-18 19:25:24 +0300

    Clarify clickable link

--- src/footer.php
+++ src/footer.php
@@ -1,4 +1,4 @@
 <div id="about-us">Drive your dreams with us!</div>
 <div id="social-media">Find us on: <ul><li><a href="https://www.facebook.com/KIKOCARSGR"><img src="/asset/facebook.png"></a></li></ul><hr class="phone"></div>
 <div id="gmaps">[google maps]<hr class="phone"></div>
-<div id="contact-bottom">Contact us for immediate assistance, 24 hours a day, 7 days a week! <ul><li><a href="tel:00306983006508">Call us on +306983006508</a></li><li>Or <a href="mailto:kikocarsgr@gmail.com">send us an email</a></div>
+<div id="contact-bottom">Contact us for immediate assistance, 24 hours a day, 7 days a week! <ul><li><a href="tel:00306983006508">Call us on +306983006508</a></li><li>Or <a href="mailto:kikocarsgr@gmail.com">click here to send us an email</a></div>
