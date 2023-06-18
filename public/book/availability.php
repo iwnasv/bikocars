3e4e3cb195f99ca7a6e02b6365d81addd33f431e
iwnasv <34813745+iwnasv@users.noreply.github.com>
2023-06-18 22:03:10 +0300

    Fix missing availability page
    Lost and found

--- public/book/availability.php
+++ public/book/availability.php
@@ -6,24 +6,15 @@ include '../src/getdates.php'; ?>
         <?php include '../../src/header.php'; ?>
     </header>
     <main>
-        <!--dwse car id (apo fleet), tsekare hmeromhnies, dialekse hmeromhnies, pane sto book-index-->
         <?php
-            if(!is_null($_GET['car'])) {
+        /* pros to paron apla zhtaei hmeromhnies kai ton petaei pisw;
+           exei polu peri8wrio gia veltiwsh, skepsou oti pernaei to car
+           me GET */
+            if($_GET['car']):
                 $_SESSION['car'] = intval(filter_input(INPUT_GET, 'car', FILTER_VALIDATE_INT));
-            } else {
-                if(is_null($_SESSION['car'])) {
-                    die('you need to select a car first');
-                }
-            }
-            echo '8es to amaksi #' . $_SESSION['car'] . ' stis ' . date('d/m/Y', $_SESSION['datestart']);
-            $statement = 'SELECT datestart, dateend FROM reservations WHERE car = ' . $_SESSION['car'] . ' AND dateend <= ' . date('Y/m/d',$_SESSION['datestart']) . ' AND datestart >= ' . date('Y/m/d',$_SESSION['dateend']);
-            $sqlobject = $db->query($statement);
-            var_dump($sqlobject->fetchArray(SQLITE3_ASSOC));
-            if(!$sqlobject->fetchArray(SQLITE3_ASSOC)){
-                echo 'this page is being rewritten... ';
-            }
-        ?>
-        <h3>για πρακτικούς λόγους και μόνο: <a href="/book/extras.php">extras</a></h3>
+            endif; ?>
+       <div>Please select pickup and return dates, so we can confirm the car's availability!</div>
+       <?php include '../../src/calendar.php' ?>
     </main>
     <footer>
         <?php include '../../src/footer.php'; ?>
